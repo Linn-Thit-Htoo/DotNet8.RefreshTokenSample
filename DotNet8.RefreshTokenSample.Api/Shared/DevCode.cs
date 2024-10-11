43312dc881416fcd428f89c9c412725ddf6deca9
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 19:22:37 +0630

    [ Added ] DevCode

---
+++ DotNet8.RefreshTokenSample.Api/Shared/DevCode.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace DotNet8.RefreshTokenSample.Api.Shared
+{
+    public static class DevCode
+    {
+        public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj);
+
+        public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
+    }
+}
