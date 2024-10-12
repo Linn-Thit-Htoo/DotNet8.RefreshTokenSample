13d27900e14b1525a13bb027bba1887a30b12c41
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:13:14 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Services/AuthServices/JWTAuth.cs
+++ DotNet8.RefreshTokenSample.Api/Services/AuthServices/JWTAuth.cs
@@ -15,13 +15,17 @@ public class JWTAuth
         {
             var claims = new[]
             {
-            new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]!),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
-            new Claim("UserId", jwtResponseModel.UserId),
-            new Claim("UserName", jwtResponseModel.UserName),
-            new Claim("Email", jwtResponseModel.Email)
-        };
+                new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(
+                    JwtRegisteredClaimNames.Iat,
+                    new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(),
+                    ClaimValueTypes.Integer64
+                ),
+                new Claim("UserId", jwtResponseModel.UserId),
+                new Claim("UserName", jwtResponseModel.UserName),
+                new Claim("Email", jwtResponseModel.Email)
+            };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
             var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
