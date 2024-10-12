a52141d9695c2cdb251980faea601140b9c6238e
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:09:43 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Features/User/UserController.cs
+++ DotNet8.RefreshTokenSample.Api/Features/User/UserController.cs
@@ -2,61 +2,60 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
-namespace DotNet8.RefreshTokenSample.Api.Features.User
+namespace DotNet8.RefreshTokenSample.Api.Features.User;
+
+[Route("api/account")]
+[ApiController]
+public class UserController : BaseController
 {
-    [Route("api/account")]
-    [ApiController]
-    public class UserController : BaseController
+    private readonly IJWTManagerRepository _jwtManagerRepository;
+    private readonly IUserRepository _userRepository;
+
+    public UserController(IJWTManagerRepository jwtManagerRepository, IUserRepository userRepository)
     {
-        private readonly IJWTManagerRepository _jwtManagerRepository;
-        private readonly IUserRepository _userRepository;
+        _jwtManagerRepository = jwtManagerRepository;
+        _userRepository = userRepository;
+    }
 
-        public UserController(IJWTManagerRepository jwtManagerRepository, IUserRepository userRepository)
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestModel loginRequest, CancellationToken cs)
+    {
+        var result = await _userRepository.LoginAsync(loginRequest, cs);
+        return Content(result);
+    }
+
+    [HttpPost("refresh-token")]
+    public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenRequestModel refreshTokenRequest, CancellationToken cs)
+    {
+        string authHeader = HttpContext.Request.Headers.Authorization!;
+        var header_token = authHeader.Split(" ");
+        var oldToken = header_token[1];
+
+        var principalResult = _jwtManagerRepository.GetClaimsPrincipalFromExpireToken(oldToken);
+        if (!principalResult.IsSuccess)
         {
-            _jwtManagerRepository = jwtManagerRepository;
-            _userRepository = userRepository;
+            return Content(principalResult);
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginRequestModel loginRequest, CancellationToken cs)
+        var principal = principalResult.Data;
+        string encryptedUserId = principal.FindFirstValue("UserId")!;
+        string encryptedUserName = principal.FindFirstValue("UserName")!;
+        string encryptedEmail = principal.FindFirstValue("Email")!;
+
+        // generate new tokens
+        var jwt = new JwtResponseModel(encryptedUserId, encryptedUserName, encryptedEmail);
+        var tokensResult = _jwtManagerRepository.GenerateTokens(jwt);
+
+        // delete refresh token
+        var deleteRefreshTokenResult = await _userRepository.DeleteRefreshTokenAsync(refreshTokenRequest.RefreshToken, refreshTokenRequest.UserId, cs);
+        if (!deleteRefreshTokenResult.IsSuccess)
         {
-            var result = await _userRepository.LoginAsync(loginRequest, cs);
-            return Content(result);
+            return Content(deleteRefreshTokenResult);
         }
 
-        [HttpPost("refresh-token")]
-        public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenRequestModel refreshTokenRequest, CancellationToken cs)
-        {
-            string authHeader = HttpContext.Request.Headers.Authorization!;
-            var header_token = authHeader.Split(" ");
-            var oldToken = header_token[1];
+        // save new refresh token
+        await _userRepository.SaveRefreshTokenAsync(refreshTokenRequest.UserId, tokensResult.Data.RefreshToken, cs);
 
-            var principalResult = _jwtManagerRepository.GetClaimsPrincipalFromExpireToken(oldToken);
-            if (!principalResult.IsSuccess)
-            {
-                return Content(principalResult);
-            }
-
-            var principal = principalResult.Data;
-            string encryptedUserId = principal.FindFirstValue("UserId")!;
-            string encryptedUserName = principal.FindFirstValue("UserName")!;
-            string encryptedEmail = principal.FindFirstValue("Email")!;
-
-            // generate new tokens
-            var jwt = new JwtResponseModel(encryptedUserId, encryptedUserName, encryptedEmail);
-            var tokensResult = _jwtManagerRepository.GenerateTokens(jwt);
-
-            // delete refresh token
-            var deleteRefreshTokenResult = await _userRepository.DeleteRefreshTokenAsync(refreshTokenRequest.RefreshToken, refreshTokenRequest.UserId, cs);
-            if (!deleteRefreshTokenResult.IsSuccess)
-            {
-                return Content(deleteRefreshTokenResult);
-            }
-
-            // save new refresh token
-            await _userRepository.SaveRefreshTokenAsync(refreshTokenRequest.UserId, tokensResult.Data.RefreshToken, cs);
-
-            return Content(tokensResult);
-        }
+        return Content(tokensResult);
     }
 }
