d4e28b0b036a9353b86dfbfa4ca95a5f2db19325
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:07:53 +0630

    -

--- DotNet8.RefreshTokenSample.Api/AppDbContextModels/AppDbContext.cs
+++ DotNet8.RefreshTokenSample.Api/AppDbContextModels/AppDbContext.cs
@@ -2,9 +2,8 @@
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
 
     public DbSet<Tbl_Login> Tbl_Login { get; set; }
     public DbSet<Tbl_User> Tbl_User { get; set; }
