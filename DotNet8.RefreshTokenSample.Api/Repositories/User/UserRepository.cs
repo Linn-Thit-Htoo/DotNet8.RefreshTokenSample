ed48510638789b6ea170b2eb6d9718a7dbdfe7e9
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:12:50 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Repositories/User/UserRepository.cs
+++ DotNet8.RefreshTokenSample.Api/Repositories/User/UserRepository.cs
@@ -6,19 +6,30 @@ public class UserRepository : IUserRepository
     private readonly AesService _aesService;
     private readonly IJWTManagerRepository _jwtManagerRepository;
 
-    public UserRepository(AppDbContext context, AesService aesService, IJWTManagerRepository jwtManagerRepository)
+    public UserRepository(
+        AppDbContext context,
+        AesService aesService,
+        IJWTManagerRepository jwtManagerRepository
+    )
     {
         _context = context;
         _aesService = aesService;
         _jwtManagerRepository = jwtManagerRepository;
     }
 
-    public async Task<Result<Tokens>> DeleteRefreshTokenAsync(string refreshToken, int userId, CancellationToken cs = default)
+    public async Task<Result<Tokens>> DeleteRefreshTokenAsync(
+        string refreshToken,
+        int userId,
+        CancellationToken cs = default
+    )
     {
         Result<Tokens> result;
         try
         {
-            var item = await _context.Tbl_Login.FirstOrDefaultAsync(x => x.RefreshToken == refreshToken && x.UserId == userId, cancellationToken: cs);
+            var item = await _context.Tbl_Login.FirstOrDefaultAsync(
+                x => x.RefreshToken == refreshToken && x.UserId == userId,
+                cancellationToken: cs
+            );
             if (item is null)
             {
                 result = Result<Tokens>.NotFound();
@@ -39,12 +50,18 @@ public class UserRepository : IUserRepository
         return result;
     }
 
-    public async Task<Result<Tokens>> LoginAsync(LoginRequestModel loginRequest, CancellationToken cs)
+    public async Task<Result<Tokens>> LoginAsync(
+        LoginRequestModel loginRequest,
+        CancellationToken cs
+    )
     {
         Result<Tokens> result;
         try
         {
-            var user = await _context.Tbl_User.FirstOrDefaultAsync(x => x.Email == loginRequest.Email && x.Password == loginRequest.Password, cancellationToken: cs);
+            var user = await _context.Tbl_User.FirstOrDefaultAsync(
+                x => x.Email == loginRequest.Email && x.Password == loginRequest.Password,
+                cancellationToken: cs
+            );
             if (user is null)
             {
                 result = Result<Tokens>.NotFound();
@@ -72,15 +89,14 @@ public class UserRepository : IUserRepository
             var user = await _context.Tbl_User.FindAsync([userId], cancellationToken: cs);
             ArgumentNullException.ThrowIfNull(user);
 
-            var login = await _context.Tbl_Login.FirstOrDefaultAsync(x => x.UserId == userId, cancellationToken: cs);
+            var login = await _context.Tbl_Login.FirstOrDefaultAsync(
+                x => x.UserId == userId,
+                cancellationToken: cs
+            );
 
             if (login is null)
             {
-                var model = new Tbl_Login
-                {
-                    UserId = userId,
-                    RefreshToken = refreshToken
-                };
+                var model = new Tbl_Login { UserId = userId, RefreshToken = refreshToken };
 
                 await _context.Tbl_Login.AddAsync(model, cs);
             }
@@ -98,8 +114,10 @@ public class UserRepository : IUserRepository
         }
     }
 
-    private JwtResponseModel GetJwtResponseModel(Tbl_User tbl_User) => new(
-        _aesService.Encrypt(tbl_User.UserId.ToString()),
-        _aesService.Encrypt(tbl_User.UserName),
-        _aesService.Encrypt(tbl_User.Email));
+    private JwtResponseModel GetJwtResponseModel(Tbl_User tbl_User) =>
+        new(
+            _aesService.Encrypt(tbl_User.UserId.ToString()),
+            _aesService.Encrypt(tbl_User.UserName),
+            _aesService.Encrypt(tbl_User.Email)
+        );
 }
