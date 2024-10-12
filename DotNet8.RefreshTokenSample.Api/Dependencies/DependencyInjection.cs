3d9e3c354ab593731ae18ab81390b88e8737fdf1
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:08:37 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Dependencies/DependencyInjection.cs
+++ DotNet8.RefreshTokenSample.Api/Dependencies/DependencyInjection.cs
@@ -7,63 +7,62 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
-namespace DotNet8.RefreshTokenSample.Api.Dependencies
+namespace DotNet8.RefreshTokenSample.Api.Dependencies;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+        return services.AddDbContextService(builder).AddDataAccessService()
+            .AddAuthenticationService(builder)
+            .AddCustomServices();
+    }
+
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            return services.AddDbContextService(builder).AddDataAccessService()
-                .AddAuthenticationService(builder)
-                .AddCustomServices();
-        }
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+        return services;
+    }
 
-            return services;
-        }
+    private static IServiceCollection AddDataAccessService(this IServiceCollection services)
+    {
+        return services.AddScoped<IUserRepository, UserRepository>().AddScoped<IJWTManagerRepository, JWTManagerRepository>();
+    }
 
-        private static IServiceCollection AddDataAccessService(this IServiceCollection services)
-        {
-            return services.AddScoped<IUserRepository, UserRepository>().AddScoped<IJWTManagerRepository, JWTManagerRepository>();
-        }
-
-        private static IServiceCollection AddAuthenticationService(
-    this IServiceCollection services,
-    WebApplicationBuilder builder
+    private static IServiceCollection AddAuthenticationService(
+this IServiceCollection services,
+WebApplicationBuilder builder
 )
-        {
-            builder
-                .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
+    {
+        builder
+            .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
                 {
-                    options.RequireHttpsMetadata = false;
-                    options.SaveToken = true;
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuer = true,
-                        ValidateLifetime = true,
-                        ValidateAudience = true,
-                        ValidAudience = builder.Configuration["Jwt:Audience"],
-                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(
-                            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
-                        )
-                    };
-                });
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+                    )
+                };
+            });
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddCustomServices(this IServiceCollection services)
-        {
-            return services.AddTransient<TokenValidationService>().AddScoped<AesService>();
-        }
+    private static IServiceCollection AddCustomServices(this IServiceCollection services)
+    {
+        return services.AddTransient<TokenValidationService>().AddScoped<AesService>();
     }
 }
