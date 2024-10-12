88325945465d8dbfe9b460acc95520b8779a837b
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:14:04 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Shared/DevCode.cs
+++ DotNet8.RefreshTokenSample.Api/Shared/DevCode.cs
@@ -1,9 +1,8 @@
-﻿namespace DotNet8.RefreshTokenSample.Api.Shared
-{
-    public static class DevCode
-    {
-        public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj);
+﻿namespace DotNet8.RefreshTokenSample.Api.Shared;
 
-        public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
-    }
+public static class DevCode
+{
+    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj);
+
+    public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
 }
