05ed16324dbf5f0083e00446f6cf503b93a2a122
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:10:29 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Features/BaseController.cs
+++ DotNet8.RefreshTokenSample.Api/Features/BaseController.cs
@@ -1,12 +1,11 @@
-﻿namespace DotNet8.RefreshTokenSample.Api.Features
+﻿namespace DotNet8.RefreshTokenSample.Api.Features;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(obj.ToJson(), "application/json");
-        }
+        return Content(obj.ToJson(), "application/json");
     }
 }
