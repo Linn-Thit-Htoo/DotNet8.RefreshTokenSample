d1fb38cca35ff652f023704c73872da3c1dbf2ad
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:14:36 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Program.cs
+++ DotNet8.RefreshTokenSample.Api/Program.cs
@@ -1,13 +1,8 @@
-using DotNet8.RefreshTokenSample.Api.AppDbContextModels;
 using DotNet8.RefreshTokenSample.Api.Dependencies;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,7 +10,6 @@ builder.Services.AddDependencyInjection(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
