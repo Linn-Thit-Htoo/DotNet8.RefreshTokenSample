fcb21ec3814674ba0fddd6a0b0c45eae687054a4
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:13:27 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Services/AuthServices/TokenValidationService.cs
+++ DotNet8.RefreshTokenSample.Api/Services/AuthServices/TokenValidationService.cs
@@ -1,45 +1,43 @@
-﻿namespace DotNet8.RefreshTokenSample.Api.Services.AuthServices
+﻿namespace DotNet8.RefreshTokenSample.Api.Services.AuthServices;
+
+public class TokenValidationService
 {
-    public class TokenValidationService
+    private readonly IConfiguration _configuration;
+
+    public TokenValidationService(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
-
-        public TokenValidationService(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        public ClaimsPrincipal ValidateToken(string token)
-        {
-            try
-            {
-                JwtSecurityTokenHandler tokenHandler = new();
-                byte[] key = Encoding.ASCII.GetBytes(_configuration.GetSection("Jwt:Key").Value!);
-
-                TokenValidationParameters parameters =
-                    new()
-                    {
-                        RequireExpirationTime = true,
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidAudience = _configuration["Jwt:Audience"],
-                        ValidIssuer = _configuration["Jwt:Issuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(key)
-                    };
-                ClaimsPrincipal principal = tokenHandler.ValidateToken(
-                    token,
-                    parameters,
-                    out SecurityToken securityToken
-                );
-
-                return principal;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
+        _configuration = configuration;
     }
 
+    public ClaimsPrincipal ValidateToken(string token)
+    {
+        try
+        {
+            JwtSecurityTokenHandler tokenHandler = new();
+            byte[] key = Encoding.ASCII.GetBytes(_configuration.GetSection("Jwt:Key").Value!);
+
+            TokenValidationParameters parameters =
+                new()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidAudience = _configuration["Jwt:Audience"],
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            ClaimsPrincipal principal = tokenHandler.ValidateToken(
+                token,
+                parameters,
+                out SecurityToken securityToken
+            );
+
+            return principal;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
 }
