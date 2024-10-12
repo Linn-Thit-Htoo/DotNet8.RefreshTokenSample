fe002b175a482578d7f55df8f31662ffb89c0ce5
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:12:30 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Repositories/User/IUserRepository.cs
+++ DotNet8.RefreshTokenSample.Api/Repositories/User/IUserRepository.cs
@@ -3,6 +3,10 @@
 public interface IUserRepository
 {
     Task SaveRefreshTokenAsync(int userId, string refreshToken, CancellationToken cs);
-    Task<Result<Tokens>> DeleteRefreshTokenAsync(string refreshToken, int userId, CancellationToken cs = default);
+    Task<Result<Tokens>> DeleteRefreshTokenAsync(
+        string refreshToken,
+        int userId,
+        CancellationToken cs = default
+    );
     Task<Result<Tokens>> LoginAsync(LoginRequestModel loginRequest, CancellationToken cs);
 }
