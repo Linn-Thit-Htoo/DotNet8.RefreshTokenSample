1a54d6eb2cdb7b2aad50ba7d9a5d08f67544e9b9
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:11:11 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Models/Tokens.cs
+++ DotNet8.RefreshTokenSample.Api/Models/Tokens.cs
@@ -1,8 +1,7 @@
-﻿namespace DotNet8.RefreshTokenSample.Api.Models
+﻿namespace DotNet8.RefreshTokenSample.Api.Models;
+
+public class Tokens
 {
-    public class Tokens
-    {
-        public string AccessToken { get; set; }
-        public string RefreshToken { get; set; }
-    }
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
 }
