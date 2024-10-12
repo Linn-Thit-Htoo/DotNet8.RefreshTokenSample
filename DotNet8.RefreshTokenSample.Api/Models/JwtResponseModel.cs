3dcc546fe2c612c5e3173bdd215f81746600113c
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:10:48 +0630

    -

--- DotNet8.RefreshTokenSample.Api/Models/JwtResponseModel.cs
+++ DotNet8.RefreshTokenSample.Api/Models/JwtResponseModel.cs
@@ -1,16 +1,15 @@
-﻿namespace DotNet8.RefreshTokenSample.Api.Models
-{
-    public class JwtResponseModel
-    {
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
+﻿namespace DotNet8.RefreshTokenSample.Api.Models;
 
-        public JwtResponseModel(string userId, string userName, string email)
-        {
-            UserId = userId;
-            UserName = userName;
-            Email = email;
-        }
+public class JwtResponseModel
+{
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+
+    public JwtResponseModel(string userId, string userName, string email)
+    {
+        UserId = userId;
+        UserName = userName;
+        Email = email;
     }
 }
