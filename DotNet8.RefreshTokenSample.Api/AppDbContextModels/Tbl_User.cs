501df5b564a1709a9e96c593d87203db3ba91e1e
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-13 00:08:23 +0630

    -

--- DotNet8.RefreshTokenSample.Api/AppDbContextModels/Tbl_User.cs
+++ DotNet8.RefreshTokenSample.Api/AppDbContextModels/Tbl_User.cs
@@ -1,14 +1,12 @@
-﻿
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace DotNet8.RefreshTokenSample.Api.AppDbContextModels
+namespace DotNet8.RefreshTokenSample.Api.AppDbContextModels;
+
+public class Tbl_User
 {
-    public class Tbl_User
-    {
-        [Key]
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
+    [Key]
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
